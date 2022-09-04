ed4c4f494aa418e9743c3322b5ea3b422ed98310
haseeb-farahi <111962155+haseeb-farahi@users.noreply.github.com>
2022-09-04 11:41:44 +0430

    Add files via upload

---
+++ MyJavascript.js
@@ -0,0 +1,104 @@
+
+
+
+// var theHead = document.getElementById('header')
+// function myFunc(){
+//     // alert('buttons clicked')
+//     theHead.style.color="red";
+
+// }
+
+// function mykeyFunc(){
+//     alert('text copied')
+// }
+
+
+// function errfunc(){
+//     alert('invalid image path')
+// }
+
+
+
+// var theDiv = document.getElementById('myDiv');
+// var showRandom = document.getElementById('showRN');
+
+// function myFuncOver(){
+//     var a = Math.random();
+
+//     var b = a*5
+//     var c = math.floor(b)+1
+//     showRandom.innerHTML=a;
+
+//  theDiv.style.color="red"
+// // var name = 'Fayaz'
+// var theImaA = document.getElementById('imgA')
+// var theImaB = document.getElementById('imgB')
+// var total = document.getElementById('totalScoreUserA')
+// var theDemo = document.getElementById('demo')
+
+// function funcUserA(){
+  
+
+//     theDemo.style.color='red'
+//     theImaA.src="./pics/2.png"
+// var num1 = Math.random();
+// var num2 = num1*6
+// var intNum = Math.floor(num2)+1
+
+// var num1x = Math.random();
+// var num2x = num1x*6
+// var intNumx = Math.floor(num2x)+1
+
+//     theImaA.src="./pics/2.png"
+    
+
+//     total.innerHTML= intNum + intNum;
+
+    
+
+
+    
+
+// }
+
+// function funcUserB(){
+
+      
+// var num1 = Math.random();
+// var num2 = num1*6
+// var intNum = Math.floor(num2)+1
+
+// var num1x = Math.random();
+// var num2x = num1*6
+// var intNumx = Math.floor(num2x)+1
+
+//     theImaA.src="./1.png"+intNum+".png"
+//     theImaB.src="./1.png"+intNum+".png"
+//     total.innerHTML= intNum + intNum;
+    
+// }
+
+  
+
+
+var btn= document.getElementById('theBtn');
+
+// function func(){
+//     alert('event func')
+
+// }
+
+btn.addEventListener('cick',myFunc );
+
+function myFunc(){
+    alert('im event listener')
+}
+
+
+var theHead = document.getElementById('pageHeader');
+
+header.addEventListener('mouseover',funcA())
+
+function funcA(){
+    alert('mouse over Header title')
+}
